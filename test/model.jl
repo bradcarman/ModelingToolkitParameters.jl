e05ce90bf1873ad91d6a424e1b0bed9a87bafcf6
Brad Carman <bradleygcarman@outlook.com>
2025-12-09 13:52:08 -0500

    v0.2.0

---
+++ test/model.jl
@@ -0,0 +1,105 @@
+using ModelingToolkit
+using ModelingToolkit: D_nounits as D, t_nounits as t
+
+@connector Pin begin
+    v(t)
+    i(t), [connect = Flow]
+end
+
+@component function Ground(; name)
+    systems = @named begin
+        g = Pin()
+    end
+    eqs = [
+        g.v ~ 0
+    ]
+    return System(eqs, t, [], []; name, systems)
+end
+
+
+Base.@kwdef mutable struct ResistorParams <: Params
+    R::Real = 1.0
+end
+
+@component function Resistor(;name)
+    systems = @named begin
+        p = Pin()
+        n = Pin()
+    end
+    pars = @parameters begin
+        R
+    end
+    eqs = [
+        p.v - n.v ~ p.i * R
+        0 ~ p.i + n.i
+    ]        
+    return System(eqs, t, [], pars; name, systems)
+end
+
+
+Base.@kwdef mutable struct CapacitorParams <: Params
+    C::Real = 0.1
+end
+
+@component function Capacitor(;name)
+    systems = @named begin
+        p = Pin()
+        n = Pin()
+    end
+    pars = @parameters begin
+        C
+    end
+    vars = @variables begin
+        v(t)=0
+    end
+    eqs = [
+        D(v) ~ p.i / C
+        v ~ p.v - n.v
+        0 ~ p.i + n.i
+    ]
+    return System(eqs, t, vars, pars; name, systems)
+end
+
+
+Base.@kwdef mutable struct ConstantVoltageParams <: Params
+    V::Real = 10.0
+end
+
+@component function ConstantVoltage(;name)
+    systems = @named begin
+        p = Pin()
+        n = Pin()
+    end
+    pars = @parameters begin
+        V
+    end
+    eqs = [
+        V ~ p.v - n.v
+        0 ~ p.i + n.i
+    ]        
+    return System(eqs, t, [], pars; name, systems)
+end
+
+
+
+@kwdef mutable struct RCModelParams <: Params
+    resistor::ResistorParams = ResistorParams()
+    capacitor::CapacitorParams = CapacitorParams()
+    source::ConstantVoltageParams = ConstantVoltageParams()
+end
+
+@component function RCModel(; name)
+    systems = @named begin
+        resistor = Resistor()
+        capacitor = Capacitor()
+        source = ConstantVoltage()
+        ground = Ground()
+    end
+    eqs = [
+        connect(source.p, resistor.p)
+        connect(resistor.n, capacitor.p)
+        connect(capacitor.n, source.n)
+        connect(capacitor.n, ground.g)
+    ]
+    return System(eqs, t, [], []; name, systems)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,110 +1,8 @@
 using Test
 using ModelingToolkit
-using ModelingToolkit: D_nounits as D, t_nounits as t
 using ModelingToolkitParameters
 
-@connector Pin begin
-    v(t)
-    i(t), [connect = Flow]
-end
-
-@component function Ground(; name)
-    systems = @named begin
-        g = Pin()
-    end
-    eqs = [
-        g.v ~ 0
-    ]
-    return System(eqs, t, [], []; name, systems)
-end
-
-
-Base.@kwdef mutable struct ResistorParams <: Params
-    R::Real = 1.0
-end
-
-@component function Resistor(;name)
-    systems = @named begin
-        p = Pin()
-        n = Pin()
-    end
-    pars = @parameters begin
-        R
-    end
-    eqs = [
-        p.v - n.v ~ p.i * R
-        0 ~ p.i + n.i
-    ]        
-    return System(eqs, t, [], pars; name, systems)
-end
-
-
-Base.@kwdef mutable struct CapacitorParams <: Params
-    C::Real = 0.1
-end
-
-@component function Capacitor(;name)
-    systems = @named begin
-        p = Pin()
-        n = Pin()
-    end
-    pars = @parameters begin
-        C
-    end
-    vars = @variables begin
-        v(t)=0
-    end
-    eqs = [
-        D(v) ~ p.i / C
-        v ~ p.v - n.v
-        0 ~ p.i + n.i
-    ]
-    return System(eqs, t, vars, pars; name, systems)
-end
-
-
-Base.@kwdef mutable struct ConstantVoltageParams <: Params
-    V::Real = 10.0
-end
-
-@component function ConstantVoltage(;name)
-    systems = @named begin
-        p = Pin()
-        n = Pin()
-    end
-    pars = @parameters begin
-        V
-    end
-    eqs = [
-        V ~ p.v - n.v
-        0 ~ p.i + n.i
-    ]        
-    return System(eqs, t, [], pars; name, systems)
-end
-
-
-
-@kwdef mutable struct RCModelParams <: Params
-    resistor::ResistorParams = ResistorParams()
-    capacitor::CapacitorParams = CapacitorParams()
-    source::ConstantVoltageParams = ConstantVoltageParams()
-end
-
-@component function RCModel(; name)
-    systems = @named begin
-        resistor = Resistor()
-        capacitor = Capacitor()
-        source = ConstantVoltage()
-        ground = Ground()
-    end
-    eqs = [
-        connect(source.p, resistor.p)
-        connect(resistor.n, capacitor.p)
-        connect(capacitor.n, source.n)
-        connect(capacitor.n, ground.g)
-    ]
-    return System(eqs, t, [], []; name, systems)
-end
+include("model.jl")
 
 @mtkcompile rc_model = RCModel()
 rc_model_params = RCModelParams()
@@ -116,5 +14,14 @@ rc_model_params.resistor.R = 2.0
 update!(prob, setters, rc_model => rc_model_params)
 
 @test prob.ps[rc_model.resistor.R] == 2.0
+
+rc_model_params.resistor.R = 3.0
+prob′ = remake(prob, setters, rc_model => rc_model_params)
+@test prob.ps[rc_model.resistor.R] == 2.0
+@test prob′.ps[rc_model.resistor.R] == 3.0
+
+
+
+
 
 ModelingToolkitParameters.params(RCModel)
